74621413b83f26f76ac13c368fd8f995b28f3999
bryanl <bryanliles@gmail.com>
2018-09-08 21:24:03 -0400

    checkpoint

---
+++ pkg/server/document_highlight.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"github.com/bryanl/jsonnet-language-server/pkg/analysis/lexical/token"
+	"github.com/bryanl/jsonnet-language-server/pkg/config"
+	"github.com/bryanl/jsonnet-language-server/pkg/lsp"
+	jpos "github.com/bryanl/jsonnet-language-server/pkg/util/position"
+	"github.com/bryanl/jsonnet-language-server/pkg/util/uri"
+)
+
+func textDocumentHighlight(r *request, c *config.Config) (interface{}, error) {
+	var params lsp.TextDocumentPositionParams
+	if err := r.Decode(&params); err != nil {
+		return nil, err
+	}
+
+	doc, err := c.Text(params.TextDocument.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	path, err := uri.ToPath(params.TextDocument.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	pos := jpos.FromLSPPosition(params.Position)
+
+	locations, err := token.Highlight(path, doc.String(), pos, c.NodeCache())
+	if err != nil {
+		return nil, err
+	}
+
+	var highlights []lsp.DocumentHighlight
+
+	for _, location := range locations {
+		r := location.Range()
+		dh := lsp.DocumentHighlight{
+			Range: r.ToLSP(),
+		}
+		highlights = append(highlights, dh)
+	}
+
+	return highlights, nil
+}
